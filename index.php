939e47e7d6b676ea8d0666eb83b1b7ad7b2204b9
Oliver_Locon <oliverloconlopez@gmail.com>
2022-09-12 12:57:49 -0600

    Pruebas

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Primer Contacto PHP</title>
+</head>
+<body>
+	<?php 
+	// No se escribe html
+	echo "<h1> Bienvenido Oli :3 </h1>";
+	echo "		TABLA DEL 5				";
+	 ?>
+
+</body>
+</html>
